a3fc6859e149f15229d71d913c1bdb8a82d35206
Nicola Szwaja <102081211+nicolaszwaja@users.noreply.github.com>
2023-04-22 14:12:36 +0200

    Add files via upload

---
+++ data_types.sql
@@ -0,0 +1,6 @@
+INT         --whole num
+DECIMAL(M,N)--total digits, digits after a coma
+VARCHAR(l)  --string of text of length l
+BLOB        --binary data
+DATE        --YYYY-MM-DD
+TIMESTAMP   --YYYY-MM-DD- HH:MM:SS
